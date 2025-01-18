d7f9ba85fce10462f2450938557af449383b8d21
VisEntities <visentities@gmail.com>
2025-01-18 20:53:09 +0100

    Add project files.

---
+++ Pop.cs
@@ -0,0 +1,181 @@
+﻿/*
+ * Copyright (C) 2024 Game4Freak.io
+ * This mod is provided under the Game4Freak EULA.
+ * Full legal terms can be found at https://game4freak.io/eula/
+ */
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oxide.Plugins
+{
+    [Info("Pop", "VisEntities", "1.0.0")]
+    [Description("Shows how many players are online, sleeping, and queued.")]
+    public class Pop : RustPlugin
+    {
+        #region Fields
+
+        private static Pop _plugin;
+        private static Configuration _config;
+
+        #endregion Fields
+
+        #region Configuration
+
+        private class Configuration
+        {
+            [JsonProperty("Version")]
+            public string Version { get; set; }
+
+            [JsonProperty("Command")]
+            public string Command { get; set; }
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            _config = Config.ReadObject<Configuration>();
+
+            if (string.Compare(_config.Version, Version.ToString()) < 0)
+                UpdateConfig();
+
+            SaveConfig();
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            _config = GetDefaultConfig();
+        }
+
+        protected override void SaveConfig()
+        {
+            Config.WriteObject(_config, true);
+        }
+
+        private void UpdateConfig()
+        {
+            PrintWarning("Config changes detected! Updating...");
+
+            Configuration defaultConfig = GetDefaultConfig();
+
+            if (string.Compare(_config.Version, "1.0.0") < 0)
+                _config = defaultConfig;
+
+            PrintWarning("Config update complete! Updated from version " + _config.Version + " to " + Version.ToString());
+            _config.Version = Version.ToString();
+        }
+
+        private Configuration GetDefaultConfig()
+        {
+            return new Configuration
+            {
+                Version = Version.ToString(),
+                Command = "pop"
+            };
+        }
+
+        #endregion Configuration
+
+        #region Oxide Hooks
+
+        private void Init()
+        {
+            _plugin = this;
+            PermissionUtil.RegisterPermissions();
+            cmd.AddChatCommand(_config.Command, this, nameof(cmdPop));
+        }
+
+        private void Unload()
+        {
+            _config = null;
+            _plugin = null;
+        }
+
+        #endregion Oxide Hooks
+
+        #region Permissions
+
+        private static class PermissionUtil
+        {
+            public const string USE = "pop.use";
+            private static readonly List<string> _permissions = new List<string>
+            {
+                USE,
+            };
+
+            public static void RegisterPermissions()
+            {
+                foreach (var permission in _permissions)
+                {
+                    _plugin.permission.RegisterPermission(permission, _plugin);
+                }
+            }
+
+            public static bool HasPermission(BasePlayer player, string permissionName)
+            {
+                return _plugin.permission.UserHasPermission(player.UserIDString, permissionName);
+            }
+        }
+
+        #endregion Permissions
+
+        #region Commands
+
+        private void cmdPop(BasePlayer player, string cmd, string[] args)
+        {
+            if (player == null)
+                return;
+
+            if (!PermissionUtil.HasPermission(player, PermissionUtil.USE))
+            {
+                MessagePlayer(player, Lang.NoPermission);
+                return;
+            }
+
+            int online = BasePlayer.activePlayerList.Count;
+            int sleepers = BasePlayer.sleepingPlayerList.Count;
+            int queued = ServerMgr.Instance.connectionQueue.Queued;
+            int adminsOnline = BasePlayer.activePlayerList.Count(p => p.IsAdmin);
+
+            MessagePlayer(player, Lang.PopulationStats, online, sleepers, queued, adminsOnline);
+        }
+
+        #endregion Commands
+
+        #region Localization
+
+        private class Lang
+        {
+            public const string NoPermission = "NoPermission";
+            public const string PopulationStats = "PopulationStats";
+        }
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                [Lang.NoPermission] = "You do not have permission to use this command.",
+                [Lang.PopulationStats] = "Players Online: {0}, Sleepers: {1}, Queued: {2}, Admins Online: {3}"
+            }, this, "en");
+        }
+
+        private static string GetMessage(BasePlayer player, string messageKey, params object[] args)
+        {
+            string message = _plugin.lang.GetMessage(messageKey, _plugin, player.UserIDString);
+
+            if (args.Length > 0)
+                message = string.Format(message, args);
+
+            return message;
+        }
+
+        public static void MessagePlayer(BasePlayer player, string messageKey, params object[] args)
+        {
+            string message = GetMessage(player, messageKey, args);
+            _plugin.SendReply(player, message);
+        }
+
+        #endregion Localization
+    }
+}
